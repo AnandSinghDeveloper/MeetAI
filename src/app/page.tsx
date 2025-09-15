427bbbccc7b10771081a46c164b96c1158e5b341
Anand Thakur <anandsingh23112005@gmail.com>
2025-09-15 09:49:12 +0530

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+"use client"
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-3xl font-semibold text-amber-700">Home</h1>
-   </div>
+    <div>
+      <h1 className="text-3xl font-semibold text-amber-700">Home</h1>
+    </div>
   );
 }
